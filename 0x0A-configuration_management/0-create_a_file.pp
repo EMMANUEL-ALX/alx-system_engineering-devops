da6772b550c0f8663fc29bf620c75a5daca681b1
EMMANUEL-ALX <emmanuelanaedobe@gmail.com>
2023-03-24 16:21:50 -0700

    included config mgt

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-# Creating a file
-file { 'holberton':
-  path    => '/tmp/holberton',
-  content => 'I love Puppet',
+# creates a file in /tmp
+
+file { '/tmp/school':
+  content =>'I love Puppet',
+  mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  mode    => '0744'
 }
